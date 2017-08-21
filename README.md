6b0f1f40fa816cbbe015fdd72fbbc5bdc1cabee5
Oleksandr Tsurenko <oleksandr.tsurenko@letscroove.com>
2017-08-21 18:11:21 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # twilio-batch-phone-numbers-remover
-Small CLI-tool that removes reserver Twilio phone numbers in batch mode
+Small CLI-tool that removes reserved Twilio.com phone numbers in batch mode
+
+Unfortunately Twilio doesn't allow you to remove phone numbers in batch mode and it is getting really tough to remove all of them if you thousands of them
+
+## Use pre-built container from Dockerhub
+```
+docker pull oleksandrua/twilio-batch-phone-numbers-remover
+docker run -e "twilio_account_sid=REPLACE_WITH_YOUR_TWILIO_ACCOUNT_SID" -e "twilio_auth_token=REPLACE_WITH_YOUR_TWILIO_AUTH_TOKEN" -it --rm oleksandrua/twilio-batch-phone-numbers-remover
+```
+
+## Or use image that was built locally
+```
+docker build -t twilio-batch-phone-numbers-remover .
+docker run -e "twilio_account_sid=REPLACE_WITH_YOUR_TWILIO_ACCOUNT_SID" -e "twilio_auth_token=REPLACE_WITH_YOUR_TWILIO_AUTH_TOKEN" -it --rm twilio-batch-phone-numbers-remover
+```
---
+++ src/app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Small CLI-tool that removes reserved Twilio.com phone numbers in batch mode
+"""
+import os
+import sys
+import time
+import twilio
+import traceback
+from twilio.rest import Client
+
+__author__ = 'Oleksandr Tsurenko (tsurenko@gmail.com)'
+__version__ = '0.1'
+
+def main():
+    try:
+        twilio_account_sid = os.environ['twilio_account_sid']
+        twilio_auth_token = os.environ['twilio_auth_token']
+
+        try:
+            client = Client(twilio_account_sid, twilio_auth_token)
+            numbers = client.incoming_phone_numbers.list()
+        except twilio.base.exceptions.TwilioException:
+            raise NameError('Wrong Twilio.com credentials')
+
+        if len(numbers) > 0:
+            print("""
+!!! WARNING WARNING WARNING!!!
+====================================================
+In 60 seconds I'm going to delete %s phone number(s).
+Ctrl/Cmd+C if you want to cancel the removal process")
+====================================================
+""" % (len(numbers)))
+            time.sleep(60)
+
+            for number in numbers:
+                print(number.phone_number, "has been successfully deleted" if number.delete() else "!!! can't delete")
+        else:
+            print("No phone numbers to delete")
+
+        print("Done")
+
+    except Exception as e:
+        raise e
+
+    sys.exit()
+
+if __name__ == '__main__':
+    try:
+        main()
+        sys.exit(0)
+
+    except KeyboardInterrupt as e:
+        raise e
+
+    except SystemExit as e:
+        raise e
+
+    except Exception as e:
+        print('ERROR, UNEXPECTED EXCEPTION')
+        print(str(e))
+        traceback.print_exc()
+        sys.exit(1)
