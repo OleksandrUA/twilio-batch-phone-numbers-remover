977bf4e856416869cbc8ee25acdf22e0eb0c517a
Oleksandr Tsurenko <oleksandr.tsurenko@letscroove.com>
2017-08-22 13:02:54 +0200

    Got rid of wrong try...except usage

--- src/app.py
+++ src/app.py
@@ -12,38 +12,34 @@ from twilio.rest import Client
 __author__ = 'Oleksandr Tsurenko (tsurenko@gmail.com)'
 __version__ = '0.1'
 
+
 def main():
+    twilio_account_sid = os.environ['twilio_account_sid']
+    twilio_auth_token = os.environ['twilio_auth_token']
+
     try:
-        twilio_account_sid = os.environ['twilio_account_sid']
-        twilio_auth_token = os.environ['twilio_auth_token']
+        client = Client(twilio_account_sid, twilio_auth_token)
+        numbers = client.incoming_phone_numbers.list()
+    except twilio.base.exceptions.TwilioException:
+        raise NameError('Wrong Twilio.com credentials')
 
-        try:
-            client = Client(twilio_account_sid, twilio_auth_token)
-            numbers = client.incoming_phone_numbers.list()
-        except twilio.base.exceptions.TwilioException:
-            raise NameError('Wrong Twilio.com credentials')
-
-        if len(numbers) > 0:
-            print("""
+    if len(numbers) > 0:
+        print("""
 !!! WARNING WARNING WARNING!!!
 ====================================================
 In 60 seconds I'm going to delete %s phone number(s).
 Ctrl/Cmd+C if you want to cancel the removal process")
 ====================================================
 """ % (len(numbers)))
-            time.sleep(60)
+        time.sleep(60)
 
-            for number in numbers:
-                print(number.phone_number, "has been successfully deleted" if number.delete() else "!!! can't delete")
-        else:
-            print("No phone numbers to delete")
+        for number in numbers:
+            print(number.phone_number, "has been successfully deleted" if number.delete() else "!!! can't delete")
+    else:
+        print("No phone numbers to delete")
 
-        print("Done")
+    print("Done")
 
-    except Exception as e:
-        raise e
-
-    sys.exit()
 
 if __name__ == '__main__':
     try:
